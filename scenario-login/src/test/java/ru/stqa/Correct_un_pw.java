ff2cd7a0bf2ed3ad27ee58a0ae09e60ace03ef6d
yunseok624 <parkyunseok624@gmail.com>
2021-11-15 13:21:27 +0300

    Scenario of login

---
+++ scenario-login/src/test/java/ru/stqa/Correct_un_pw.java
@@ -0,0 +1,38 @@
+package ru.stqa;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class Correct_un_pw {
+    private WebDriver driver;
+
+    @Before
+    public void start() {
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @Test
+    public void check() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\parky\\Documents\\GitHub\\Making_Infrastructure\\scenario-login\\chromedriver.exe");
+        driver.get("http://localhost/litecart/admin/login.php?redirect_url=%2Flitecart%2Fadmin%2F");
+        WebElement username = driver.findElement(By.name("username"));
+        username.sendKeys("admin");
+        WebElement password = driver.findElement(By.name("password"));
+        password.sendKeys("admin");
+        driver.findElement(By.name("login")).click();
+    }
+
+    @After
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+}
