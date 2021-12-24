40fac212ad40c9e34cd1ae87f40c1433b3f87b81
yunseok624 <parkyunseok624@gmail.com>
2021-12-25 02:29:16 +0300

    Update #task13

---
+++ check_trashbin/src/test/java/ru/stqa/checkBin.java
@@ -0,0 +1,100 @@
+package ru.stqa;
+
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.textToBePresentInElement;
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class checkBin {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void start() {
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        driver.get("http://localhost/litecart/en/");
+        wait.until(titleIs("Online Store | My Store"));
+        for (int i = 0; i < 3; i++) {
+            addItemToCart();
+        }
+        driver.findElement(By.cssSelector("div#cart a.link")).click();
+        clearCart();
+    }
+
+    private void addItemToCart() {
+        int number = Integer.parseInt(driver.findElement(By.cssSelector("span.quantity")).getText());
+        WebElement element = driver.findElement(By.cssSelector("div.content div.name"));
+        Assert.assertNotNull("No products found on main page", element);
+        element.click();
+        if (!isElementNotPresent(driver, By.cssSelector("td.options")) ) {
+            Select size = new Select(driver.findElement(By.cssSelector("select[name='options[Size]']")));
+            JavascriptExecutor js = (JavascriptExecutor) driver;
+            js.executeScript("arguments[0].selectedIndex = 1; arguments[0].dispatchEvent(new Event('change'))",
+                    size);
+        }
+        implicitlyWaitOff();
+        driver.findElement(By.cssSelector("button[name='add_cart_product']")).click();
+        wait.until(textToBePresentInElement(driver.findElement(By.cssSelector("span.quantity")),
+                String.valueOf(number + 1)));
+        implicitlyWaitOn();
+        driver.navigate().back();
+    }
+
+    private void clearCart(){
+        int count = driver.findElements(By.cssSelector("li.shortcut")).size();
+        for (int i = 0; i < count; i++) {
+            driver.findElement(By.cssSelector("li.shortcut")).click();
+            driver.findElement(By.name("remove_cart_item")).click();
+            implicitlyWaitOff();
+            wait.until(ExpectedConditions.numberOfElementsToBeLessThan(By.cssSelector
+                    ("div#box-checkout-summary td.item"), i));
+            implicitlyWaitOn();
+        }
+        driver.findElement(By.name("remove_cart_item")).click();
+        implicitlyWaitOff();
+        wait.until(ExpectedConditions.stalenessOf(driver.findElement(By.id("box-checkout-summary"))));
+        implicitlyWaitOn();
+    }
+
+    private void implicitlyWaitOn() {
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    private void implicitlyWaitOff() {
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(0));
+    }
+
+    boolean isElementNotPresent(WebDriver driver, By locator) {
+        try {
+            implicitlyWaitOff();
+            return driver.findElements(locator).size() == 0;
+        } finally {
+            implicitlyWaitOn();
+        }
+    }
+
+    @After
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+}
